337d54299824d939fb63d09b09f4b88a59f143c4
kewineic <kewin.ferreira@hotmail.com>
2020-04-29 06:24:29 -0300

    Implementado sistema de placar

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var pincel = tela.getContext('2d');
 var eixoXbolinha = 300;
 var eixoYbolinha = 200;
 var diametro = 22;
-var raio = diametro/2;
+var raio = diametro / 2;
 
 var velocidadeX = 2;
 var velocidadeY = 2;
@@ -22,79 +22,104 @@ var teclaBaixo = 40;
 
 var taxaTeclado = 20;
 
-function limpaTela(){
+var meusPontos = 0;
+var rivalPontos = 0;
+
+function limpaTela() {
     pincel.clearRect(0, 0, 600, 400);
 }
 
-function verificaColisaoBordas(){
-    if( eixoXbolinha > (tela.width - diametro) || eixoXbolinha < diametro ){
-        velocidadeX*=-1;
+function verificaColisaoBordas() {
+    if (eixoXbolinha > (tela.width - diametro) || eixoXbolinha < diametro) {
+        velocidadeX *= -1;
     }
-    
-    if( eixoYbolinha > (tela.height - diametro) || eixoYbolinha < diametro ){
-        velocidadeY*=-1;
+
+    if (eixoYbolinha > (tela.height - diametro) || eixoYbolinha < diametro) {
+        velocidadeY *= -1;
     }
 }
 
-function verificaColisaoBolaVsRaquete(){
-    if( 
-        (eixoXbolinha - diametro) < (eixoXraquete + 10)  &&
-        (eixoYbolinha - diametro) > (eixoYraquete - 50) &&  
-        (eixoYbolinha - diametro) < (eixoYraquete + 100) 
-    ){
-        velocidadeX*=-1;
+function verificaColisaoBolaVsRaquete() {
+    if (
+        (eixoXbolinha - diametro) < (eixoXraquete + 10) &&
+        (eixoYbolinha - diametro) > (eixoYraquete - 50) &&
+        (eixoYbolinha - diametro) < (eixoYraquete + 100)
+    ) {
+        velocidadeX *= -1;
     }
-    
+
 }
 
-function verificaColisaoBolaVsRaqueteRival(){
-    if( 
-        (eixoXbolinha + diametro) > (eixoXraqueteRival )  &&
-        (eixoYbolinha + diametro) > (eixoYraqueteRival - 50) &&  
-        (eixoYbolinha + diametro) < (eixoYraqueteRival + 100) 
-    ){
-        velocidadeX*=-1;
+function verificaColisaoBolaVsRaqueteRival() {
+    if (
+        (eixoXbolinha + diametro) > (eixoXraqueteRival) &&
+        (eixoYbolinha + diametro) > (eixoYraqueteRival - 50) &&
+        (eixoYbolinha + diametro) < (eixoYraqueteRival + 100)
+    ) {
+        velocidadeX *= -1;
     }
-    
+
 }
 
-function desenhaBolinha(x, y, diam){
+function verificaPontuacao() {
+    if (eixoXbolinha < diametro) {
+        rivalPontos++;
+    } else if (eixoXbolinha > (tela.width - diametro)) {
+        meusPontos++;
+    }
+}
+
+function desenhaBolinha(x, y, diam) {
     pincel.fillStyle = 'white';
     pincel.beginPath();
     pincel.arc(x, y, diam, 0, 2 * Math.PI);
     pincel.fill();
 }
 
-function direcaoBolinha(){
-    eixoXbolinha+=velocidadeX;
-    eixoYbolinha+=velocidadeY;
+function direcaoBolinha() {
+    eixoXbolinha += velocidadeX;
+    eixoYbolinha += velocidadeY;
 }
 
-function desenhaRaquete(){    
+function desenhaRaquete() {
     pincel.fillStyle = 'white';
     pincel.fillRect(eixoXraquete, eixoYraquete, 10, 100);
 }
 
-function desenhaRaqueteRival(){    
+function desenhaRaqueteRival() {
     pincel.fillStyle = 'white';
     pincel.fillRect(eixoXraqueteRival, eixoYraqueteRival, 10, 100);
 }
 
-function movimentoTeclado(evento){
-    if(evento.keyCode == teclaCima){
+function movimentoTeclado(evento) {
+    if (evento.keyCode == teclaCima) {
         eixoYraquete -= taxaTeclado
-        
-    }else if(evento.keyCode == teclaBaixo){
-        eixoYraquete += taxaTeclado   
+
+    } else if (evento.keyCode == teclaBaixo) {
+        eixoYraquete += taxaTeclado
     }
     atualizaTela();
 }
 
-function movimentoRival(){
+function movimentoRival() {
     eixoYraqueteRival = eixoYbolinha - 50;
 }
 
-function atualizaTela(){
+function mostraPontuacao(){
+    pincel.font = "bold 16pt Arial";
+    pincel.textAlign = 'center';
+    pincel.fillStyle = 'orange';
+    pincel.strokeStyle = "white";
+    pincel.fillRect(230, 8, 40, 20);
+    pincel.strokeRect(230, 8, 40, 20);
+    pincel.fillRect(330, 8, 40, 20);
+    pincel.strokeRect(330, 8, 40, 20);
+    pincel.fillStyle = 'white';
+    pincel.fillText(meusPontos, 250, 26);
+    pincel.fillText(rivalPontos, 350, 26);
+}
+
+function atualizaTela() {
     limpaTela();
     desenhaRaquete();
     desenhaRaqueteRival();
@@ -102,8 +127,11 @@ function atualizaTela(){
     verificaColisaoBordas();
     verificaColisaoBolaVsRaquete();
     verificaColisaoBolaVsRaqueteRival();
+    verificaPontuacao();
+    mostraPontuacao();
     direcaoBolinha();
-    desenhaBolinha(eixoXbolinha, eixoYbolinha, diametro);  
+    desenhaBolinha(eixoXbolinha, eixoYbolinha, diametro);
+    console.log(rivalPontos);
 }
 
 document.onkeydown = movimentoTeclado;
